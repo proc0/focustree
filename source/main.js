6ed4bcffde25056a08a725cea47e12f38e0e8311
proc0 <alanvx@gmail.com>
2025-07-18 13:54:01 -0700

    adding recursive slots, adding some styles, adding add task button

--- source/main.js
+++ source/main.js
@@ -1,20 +1,15 @@
-customElements.define(
-  'element-details',
-  class extends HTMLElement {
-    constructor() {
-      super()
-      const template = document.getElementById(
-        'element-details-template'
-      ).content
-      this.attachShadow({ mode: 'open' }).appendChild(template.cloneNode(true))
-    }
-  }
-)
+const taskModel = {
+  task_id: 0,
+  task_name: 'New Task',
+  task_note: 'This is a new task.',
+  task_fields: [],
+  task_subs: [],
+}
 
-const testObj = {
+const taskTests = {
   task_id: '1',
   task_name: 'first task',
-  task_details: 'first task details',
+  task_note: 'first task details',
   task_fields: ['field1', 'field2'],
   field1: 'value1',
   field2: 'value2',
@@ -22,7 +17,7 @@ const testObj = {
     {
       task_id: '2',
       task_name: 'second subtask',
-      task_details: 'second task details',
+      task_note: 'second task details',
       task_fields: ['field3', 'field4'],
       field3: 'value3',
       field4: 'value4',
@@ -30,7 +25,7 @@ const testObj = {
         {
           task_id: '4',
           task_name: 'sub subtask',
-          task_details: 'sub sub task details',
+          task_note: 'sub sub task details',
           task_fields: ['field3.1', 'field4.1'],
           field3: 'value3.1',
           field4: 'value4.1',
@@ -41,7 +36,7 @@ const testObj = {
     {
       task_id: '3',
       task_name: 'third subtask',
-      task_details: 'third task details',
+      task_note: 'third task details',
       task_fields: ['field5', 'field6'],
       field3: 'value5',
       field4: 'value6',
@@ -50,26 +45,53 @@ const testObj = {
   ],
 }
 
-function renderTaskTree(task) {
-  const taskElement = document.createElement('element-details')
-  const taskTitle = document.createElement('div')
-  taskTitle.setAttribute('slot', 'title')
-  taskTitle.textContent = task.task_name
-  taskElement.appendChild(taskTitle)
+customElements.define(
+  'task-element',
+  class extends HTMLElement {
+    constructor() {
+      super()
+      const template = document.getElementById('task-template').content
+      this.attachShadow({ mode: 'open' }).appendChild(template.cloneNode(true))
+    }
+  }
+)
 
-  const taskDetails = document.createElement('div')
-  taskDetails.setAttribute('slot', 'description')
-  taskDetails.textContent = task.task_details
-  taskElement.appendChild(taskDetails)
+function renderTaskTree(task) {
+  const taskName = document.createElement('span')
+  taskName.setAttribute('slot', 'task-name')
+  taskName.textContent = task.task_name
+
+  const taskNote = document.createElement('span')
+  taskNote.setAttribute('slot', 'task-note')
+  taskNote.textContent = task.task_note
+
+  const taskElement = document.createElement('task-element')
+  taskElement.appendChild(taskName)
+  taskElement.appendChild(taskNote)
+
+  if (!task.task_subs?.length) {
+    const leafSubTasks = document.createElement('span')
+    leafSubTasks.setAttribute('slot', 'task-leaf')
+    taskElement.shadowRoot
+      .getElementById('task-head')
+      .setAttribute('class', 'task-leaf')
+    taskElement.appendChild(leafSubTasks)
+    return taskElement
+  }
 
   for (const sub in task.task_subs) {
-    const subtask = renderTaskTree(task.task_subs[sub])
-    subtask.setAttribute('slot', 'attributes')
-    taskElement.appendChild(subtask)
+    const subTask = renderTaskTree(task.task_subs[sub])
+    subTask.setAttribute('slot', 'task-subs')
+    taskElement.appendChild(subTask)
   }
+
   return taskElement
 }
 
-const main = document.getElementsByTagName('main')[0]
+const addTaskButton = document.getElementById('add-task')
+addTaskButton.addEventListener('click', () => {
+  main.appendChild(renderTaskTree(taskModel))
+})
 
-main.appendChild(renderTaskTree(testObj))
+const main = document.getElementsByTagName('main')[0]
+main.appendChild(renderTaskTree(taskTests))
